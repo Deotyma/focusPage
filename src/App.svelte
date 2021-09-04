545e6b1b5b70dd1593c0f0d80667aaf016a0f756
Ewa <kalistaewa@gmail.com>
2021-09-04 15:55:44 +0200

    FOCUS

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>FOCUS c'est une page de l'association FOCUS</p>
 </main>
 
 <style>
