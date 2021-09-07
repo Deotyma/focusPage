e0a0c352e2a97543f0c0539c48d2f48d5434c1e7
Ewa <kalistaewa@gmail.com>
2021-09-07 21:32:31 +0200

    adds router and links

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,20 @@
 <script>
-	export let name;
 	import NavBar from "./Components/NavBar.svelte";
+	import Home from "./Vues/Home.svelte";
+	import MOC from "./Vues/MOC.svelte";
+	import Recherches from "./Vues/Recherches.svelte";
+	import Blog from "./Vues/Blog.svelte";
+	import {Router, Route, Link} from "svelte-routing";
 </script>
 
+<Router>
 	<NavBar />
-
+	<Route path="/" component={Home}/>
+	<Route path="/home" component={Home}/>
+	<Route path="/moc" component={MOC}/>
+	<Route path="/recherches" component={Recherches}/>
+	<Route path="/blog" component={Blog}/>
+</Router>
 
 <style>
 	
